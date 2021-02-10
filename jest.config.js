6feff4ac6377649abb6fec795187e4fa89ec7915
Henrique Mateus Vieira dos S. Silva <henrique.silva@fbest.org.br>
2021-02-10 10:58:23 -0300

    Desafio-Fundamentos-react-native

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 // For a detailed explanation regarding each configuration property, visit:
-https://jestjs.io/docs/en/configuration.html
+
 
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
-
+  https://jestjs.io/docs/en/configuration.html
   // Stop running tests after `n` failures
   // bail: 0,
 
